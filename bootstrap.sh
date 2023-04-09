ec05db07170b5153c5f4a274183a601db6e0042e
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2023-04-08 20:23:30 -0500

    feat:add bootstrap file

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+mvn -U io.quarkus:quarkus-maven-plugin:create \
+        -DprojectGroupId=org.agoncal.quarkus.starting \
+        -DprojectArtifactId=rest-book \
+        -DclassName="org.agoncal.quarkus.starting.BookResource" \
+        -Dpath="/api/books" \
+        -Dextensions="resteasy-jsonb"
